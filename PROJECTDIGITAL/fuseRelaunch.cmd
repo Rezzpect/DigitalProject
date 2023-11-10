8d44d01402bea1e8d1e6938b7b934497d1d89f71
Rez.pec <apecchintathum@gmail.com>
2023-11-10 18:11:12 +0700

    YGtbgt

---
+++ PROJECTDIGITAL/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "C:/DG/PROJECTDIGITAL/main_isim_beh.exe" -prj "C:/DG/PROJECTDIGITAL/main_beh.prj" "work.main" 
--- PROJECTDIGITAL/main.vhd
+++ PROJECTDIGITAL/main.vhd
@@ -21,6 +21,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,9 +33,10 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity main is
-	port (
+	port(
 			P_LEFT : in std_logic;
 			P_RIGHT : in std_logic;
+			START : in std_logic;
 
 			CLOCK : in std_logic;
 			HSYNC : out std_logic;
@@ -42,6 +44,19 @@ entity main is
 			RED : out std_logic;
 			GREEN : out std_logic;
 			BLUE : out std_logic;
+			
+         A : out std_logic;
+         B : out std_logic;
+         C : out std_logic;
+         D : out std_logic;
+         E : out std_logic;
+         F : out std_logic;
+         G : out std_logic;
+			
+			C0 : out std_logic;
+			C1 : out std_logic;
+			C2 : out std_logic;
+			C3 : out std_logic;
 
 			BUZZER : out std_logic
 		);
@@ -74,6 +89,7 @@ architecture Behavioral of main is
 	
 	constant brick_row : natural := 6;
 	constant brick_column : natural := 5;
+	constant brick_num : natural := brick_row*brick_column;
 	
 	---- RECORD ----
 	
@@ -105,7 +121,7 @@ begin
 			box2 : box_entity) return boolean is
 		begin 
 			if (box1.x + box1.width >= box2.x and box1.x + box1.width <= box2.x + box2.width) or (box1.x >= box2.x and box1.x <= box2.x + box2.width) then
-				if (box1.y + box1.height >= box2.y and box1.y + box1.height <= box2.y + box2.height) or (box1.y + box1.height >= box2.y and box1.y + box1.height <= box2.y + box2.height) then
+				if (box1.y + box1.height >= box2.y and box1.y + box1.height <= box2.y + box2.height) or (box1.y >= box2.y and box1.y <= box2.y + box2.height) then
 					return true;
 				end if;
 			end if;
@@ -147,6 +163,21 @@ begin
 			GREEN <= color_c.g;
 			BLUE <= color_c.b;
 		end procedure;
+
+        procedure set_seven(
+            num : natural) is 
+            variable BCD : std_logic_vector(3 downto 0);
+        begin
+            BCD := std_logic_vector(to_unsigned(num,4));
+            A <= BCD(0) OR BCD(2) OR (BCD(1) AND BCD(3)) OR (NOT BCD(1) AND NOT BCD(3));
+            B <= (NOT BCD(1)) OR (NOT BCD(2) AND NOT BCD(3)) OR (BCD(2) AND BCD(3));
+            C <= BCD(1) OR NOT BCD(2) OR BCD(3);
+            D <= (NOT BCD(1) AND NOT BCD(3)) OR (BCD(2) AND NOT BCD(3)) OR (BCD(1) AND NOT BCD(2) AND BCD(3)) OR (NOT BCD(1) AND BCD(2)) OR BCD(0);
+            E <= (NOT BCD(1) AND NOT BCD(3)) OR (BCD(2) AND NOT BCD(3));
+            F <= BCD(0) OR (NOT BCD(2) AND NOT BCD(3)) OR (BCD(1) AND NOT BCD(2)) OR (BCD(1) AND NOT BCD(3));
+            G <= BCD(0) OR (BCD(1) AND NOT BCD(2)) OR ( NOT BCD(1) AND BCD(2)) OR (BCD(2) AND NOT BCD(3));
+        end procedure;
+
 			
 		procedure draw_shape(
 			shape : box_entity;
@@ -172,9 +203,60 @@ begin
 				set_color((r=>'1',g=>'1',b=>'1'));
 			end if;
 		end procedure;
-			
+
+        procedure draw_hp(
+            hp : natural;
+            cord_x : natural;
+            cord_y : natural) is
+			variable temp_x :natural;
+        begin 
+			temp_x := cord_x;
+            --L--
+            draw_shape((x => temp_x,y => cord_y,vx => 0,vy => 0,width => 12,height => 16),(r=>'1',g=>'1',b=>'1'),false);
+            draw_shape((x => temp_x + 4,y => cord_y,vx => 0,vy => 0,width => 8,height => 12),(r=>'0',g=>'0',b=>'0'),false);
+            --I--
+            temp_x := temp_x + 12 +2;
+            draw_shape((x =>  temp_x,y => cord_y,vx => 0,vy => 0,width => 12,height => 16),(r=>'1',g=>'1',b=>'1'),false);
+            draw_shape((x => temp_x,y => cord_y+4,vx => 0,vy => 0,width => 4,height => 8),(r=>'0',g=>'0',b=>'0'),false);
+            draw_shape((x => temp_x + 8,y => cord_y+4,vx => 0,vy => 0,width => 4,height => 8),(r=>'0',g=>'0',b=>'0'),false);
+            --V--
+            temp_x := temp_x + 12 +2;
+            draw_shape((x => temp_x,y => cord_y,vx => 0,vy => 0,width => 12,height => 16),(r=>'1',g=>'1',b=>'1'),false);
+            draw_shape((x => temp_x,y => cord_y+12,vx => 0,vy => 0,width => 4,height => 4),(r=>'0',g=>'0',b=>'0'),false);
+            draw_shape((x => temp_x + 4,y => cord_y,vx => 0,vy => 0,width => 4,height => 12),(r=>'0',g=>'0',b=>'0'),false);
+            draw_shape((x => temp_x + 8,y => cord_y+12,vx => 0,vy => 0,width => 4,height => 4),(r=>'0',g=>'0',b=>'0'),false);
+            --E--
+            temp_x :=  temp_x + 12 +2;
+            draw_shape((x => temp_x,y => cord_y,vx => 0,vy => 0,width => 12,height => 16),(r=>'1',g=>'1',b=>'1'),false);
+            draw_shape((x => temp_x + 4,y => cord_y+4,vx => 0,vy => 0,width => 8,height => 3),(r=>'0',g=>'0',b=>'0'),false);
+            draw_shape((x => temp_x + 4,y => cord_y+7,vx => 0,vy => 0,width => 8,height => 3),(r=>'0',g=>'0',b=>'0'),false);
+
+            temp_x := temp_x + 16;
+            for i in 0 to hp loop
+                draw_shape((x => temp_x,y => cord_y,vx => 0,vy => 0,width => 6,height => 16),(r=>'1',g=>'0',b=>'0'),false);
+            end loop;
+        end procedure;
+
+			procedure set_common(
+				digit_num : natural) is
+			begin
+				C0 <= '1';
+				C1 <= '1';
+				C2 <= '1';
+				C3 <= '1';
+				if digit_num = 0 then
+					C0 <= '0';
+				elsif digit_num = 1 then
+					C1 <= '0';
+				elsif digit_num = 2 then
+					C2 <= '0';
+				else
+					C3 <= '0';
+				end if;
+			end procedure;
+
 		---- VARIABLE ----
-			
+	
 		variable player : box_entity := (
 			x => (screen_width/2)- 48,
 			y => ((2*screen_height)/3) + 40 ,
@@ -189,8 +271,8 @@ begin
 			y => 0,
 			vx => 0 ,
 			vy => 0 ,
-			width => 96,
-			height => 20
+			width => 100,
+			height => 22
 		);
 			
 		constant default_brick_hp : brick_life :=(life => 2);
@@ -204,12 +286,12 @@ begin
 			y => 0,
 			vx => 0 ,
 			vy => 0 ,
-			width => 96,
-			height => 20
+			width => 100,
+			height => 22
 		);
 		
 		variable ball : box_entity := (
-			x => screen_width/2,
+			x => (screen_width/2) - 4,
 			y => (2*screen_height)/3,
 			vx => -1 ,
 			vy => -1 ,
@@ -218,6 +300,14 @@ begin
 		);
 		
 		variable game_delay : natural range 0 to 1000000 := 0;
+      	variable score : natural range 0 to 130 :=0;
+      	variable health : natural range 0 to 3 := 3;
+      	variable common : natural range 0 to 3 := 3;
+    	variable temp_score : natural := 0;
+      	variable c_count : natural range 0 to 100000:=0;
+      	variable digit_score : natural := 0;
+		variable game_start : boolean := false;
+		variable brick_counter : natural range 0 to brick_num := 0;
 		
 	begin 
 		
@@ -225,24 +315,19 @@ begin
 			RED <= '0';
 			GREEN <= '0';
 			BLUE <= '0';
+			set_common(common);
 			
 			game_delay := game_delay + 1;
 			if game_delay = 100000 then
+				--pause game--	
+				if START ='1' then
+					game_start := true;
+				else
+					game_start := false;
+				end if; 
+
 				game_delay := 0;
 				---- update game ----
-				--player movement--
-				player.vx := 2*(to_integer(P_RIGHT)-to_integer(P_LEFT));
-				if player.x > 1 and player.x < screen_width-player.width then
-					player.x := player.x + player.vx;
-				end if;
-				
-				if player.x < 1 then
-					player.x := 2;
-				end if;
-				if player.x > screen_width-player.width then
-					player.x := screen_width-player.width-1;
-				end if;
-				
 				--collide with left and right wall--
 				if ball.x <= 1 or ball.x >= screen_width - ball.width then
 					ball.vx := 0 - ball.vx;
@@ -254,15 +339,84 @@ begin
 				--collide with player--
 				ball := collide(ball,player);
 				
-				--move ball--
-				ball.x := ball.x + ball.vx;
-				ball.y := ball.y + ball.vy;
+
+				--When game is not on pause -> do these thing--
+				if game_start then
+					--player movement--
+					player.vx := 2*(to_integer(P_RIGHT)-to_integer(P_LEFT));
+					if player.x > 1 and player.x < screen_width-player.width then
+						player.x := player.x + player.vx;
+					end if;
 				
+					if player.x < 1 then
+						player.x := 2;
+					end if;
+					if player.x > screen_width-player.width then
+						player.x := screen_width-player.width-1;
+					end if;
+						
+					--move ball--
+					ball.x := ball.x + ball.vx;
+					ball.y := ball.y + ball.vy;
+				end if;
 			end if;
 			draw_ball(ball);
 			
 			draw_shape(player,(r=>'1',g=>'1',b=>'1'),false);
+
+         draw_hp(health,screen_width*3/4,4);
+
+			--collide with brick--
+			brick.x := (brick_counter mod brick_column)*(brick.width + 2);
+			brick.y := 32 + (brick_counter / brick_column)*(brick.height +2);
+			if brick_list(brick_counter).life > 0 and is_collide(ball,brick) then
+				ball := collide(ball,brick);
+				brick_list(brick_counter).life:=  brick_list(brick_counter).life - 1;
+				if brick_list(brick_counter).life > 0 then
+					score := score + 1;
+				else
+					score := score + 3;
+				end if;
+				ball.x := ball.x + ball.vx;
+				ball.y := ball.y + ball.vy;
+			end if;
 			
+          	-- draw brick--
+
+			for i in 0 to brick_row -1 loop
+				for j in 0 to brick_column - 1 loop
+					brick_for_display.x := (j mod brick_column)*(brick_for_display.width + 2);
+					brick_for_display.y := 32 + (i mod brick_row)*(brick_for_display.height +2);
+					if brick_list((5*i)+j).life = 2 then
+						 draw_shape(brick_for_display,(r=>'1',g=>'1',b=>'1'),false);
+					end if;
+					if brick_list((5*i)+j).life = 1 then
+						 draw_shape(brick_for_display,(r=>'1',g=>'0',b=>'0'),false);
+					end if;
+				end loop;
+			end loop;
+			
+				
+            -- 7 segment --
+            temp_score := score;
+            c_count := c_count + 1;
+            if c_count = 100000 then
+                c_count := 0;
+                if common = 0 then
+                    temp_score := (temp_score mod 10);
+                    digit_score := temp_score;
+                elsif common = 1 then
+                    temp_score := (temp_score mod 100);
+                    digit_score := temp_score/10;
+                elsif common = 2 then
+                    digit_score := temp_score/100;
+                elsif common = 3 then
+                    digit_score := 0;
+                end if;
+                set_seven(digit_score);
+            end if;
+
+            
 			-- Hsync and Vsync
 			if x > 0 and x <= X_SYNC_PULSE then
 				HSYNC <= '0';
@@ -283,9 +437,20 @@ begin
 				x <= 0;
 			end if;
 
+        common := common +1 ;
+
+        if common > 3 then
+            common := 0;
+        end if;
+
 			if y = Y_WHOLE_FRAME then
 				y <= 0;
 			end if;
+			
+			brick_counter := brick_counter + 1;
+				if brick_counter = brick_num then
+					brick_counter := 0;
+				end if;
 		end if;
 	end process;
 
